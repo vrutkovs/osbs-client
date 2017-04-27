bffd08041b2c35aff924a1b5356d6c9df803787f
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-04-27 16:38:04 +0200

    tito

--- osbs/__init__.py
+++ osbs/__init__.py
@@ -1,1 +1,35 @@
-__version__ = "0.38"
+"""
+Copyright (c) 2015 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+from __future__ import print_function, absolute_import, unicode_literals
+
+import logging
+
+from atomic_reactor.version import __version__
+
+
+def set_logging(name="osbs", level=logging.DEBUG):
+    # create logger
+    logger = logging.getLogger(name)
+    logger.handlers = []
+    logger.setLevel(level)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+
+set_logging(level=logging.WARNING)  # override this however you want
